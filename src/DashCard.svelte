4c3a0046b466188d2e4bc59d81bc3be929bd3e3b
arlohb <arlo@urwick.co.uk>
2023-02-25 22:05:40 +0000

    Added flowbite-svelte ui framework

---
+++ src/DashCard.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Card, P, Button } from "flowbite-svelte";
+
+  let textChanged = true;
+  $: text = textChanged ? "Hello!" : "World!";
+</script>
+
+<Card class="space-y-4">
+  <Button
+    pill
+    gradient
+    color="purpleToPink"
+    shadow="purple"
+    on:click={() => (textChanged = !textChanged)}>Change</Button
+  >
+  <P>{text}</P>
+</Card>
